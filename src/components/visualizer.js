a89eed1f0f3f48627ef3ade9095605adffe654a3
MichOchieng <MichOchieng@gmail.com>
2021-07-19 15:38:53 -0700

    Added logic for generateArray function

--- src/components/visualizer.js
+++ src/components/visualizer.js
@@ -4,18 +4,25 @@ export default class Visualizer extends Component{
     constructor(props){
         super(props);
         this.state = {
-            array: [] // Will be the array used for sorting algorithms
+            array: [], // Will be the array used for sorting algorithms
+            arraySize: 100
         }
         this.generateArray = this.generateArray.bind(this);
     }
 
     componentDidMount(){
         // Will generate the initial array once the page loads
-        this.generateArray(this.state.array);
+        this.generateArray();
     }
 
-    generateArray(arr){
-        console.log('Creating array');
+    generateArray(){
+        const array = this.state.array;
+        for (let index = 0; index < this.state.arraySize; index++) {
+            // Will push a random value from 3 - 1000 into array
+            // https://stackoverflow.com/a/7228322
+            array.push(Math.floor(Math.random() * (1000 - 3 + 1) + 3));
+        }
+        this.setState({array});
     }
 
     render(){
