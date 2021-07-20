125d455c7d615cf99634de244078194d33fe98da
MichOchieng <MichOchieng@gmail.com>
2021-07-20 14:34:55 -0700

    Removed navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {Container} from 'react-bootstrap';
-import Nav from './components/nav';
 import Visualizer from './components/visualizer';
 
 import './styles/App.css';
@@ -9,7 +8,6 @@ function App() {
   return (
     <div className="App">
       <Container>
-        <Nav></Nav>
         <Visualizer></Visualizer>
       </Container>
     </div>
