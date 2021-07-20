386fb09156c4fe248ff43226fcc9612fa876775a
MichOchieng <MichOchieng@gmail.com>
2021-07-20 14:35:29 -0700

    Added navbar component to simplify array sorting and generation

--- src/components/visualizer.js
+++ src/components/visualizer.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import {Navbar,Container,Nav} from 'react-bootstrap';
+import Button from 'react-bootstrap/esm/Button';
 import '../styles/visualizer.css';
 
 export default class Visualizer extends Component{
@@ -17,7 +19,8 @@ export default class Visualizer extends Component{
     }
 
     generateArray(){
-        const array = this.state.array;
+        var array = this.state.array;
+        array = [];
         for (let index = 0; index < this.state.arraySize; index++) {
             // Will push a random value from 3 - 1000 into array
             // https://stackoverflow.com/a/7228322
@@ -29,8 +32,9 @@ export default class Visualizer extends Component{
     render(){
         const mappingArray = this.state.array;
         return(
-            // Render array values to divs
-            mappingArray.map( (value,idx) => (
+            <div>
+            {/* Render array values to divs */}
+            {mappingArray.map( (value,idx) => (
                 <div 
                     class="sortingBar" 
                     key={idx}
@@ -38,7 +42,23 @@ export default class Visualizer extends Component{
                 >
                 </div>
                 )
-            )
+            )}
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Container>
+                    <Button variant="info" onClick={() => {this.generateArray()}}>Refresh Array</Button>
+                    <Button variant="success">Sort</Button>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <Button variant="info">test</Button>
+                            <Nav.Link href="#pricing">test</Nav.Link>
+                        </Nav>
+                        
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+            
+            </div>
         );
     }
 }
