075bb03b665448749a41d6e6de32ce8c3cd850e8
MichOchieng <MichOchieng@gmail.com>
2021-07-21 22:06:10 -0700

    Added mergesort function and alrogithm selection to nav bar, also removed unused imports

--- src/components/visualizer.js
+++ src/components/visualizer.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
-import {Navbar,Container,Nav} from 'react-bootstrap';
-import Button from 'react-bootstrap/esm/Button';
+import {Navbar,Container} from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import {FaPlayCircle,FaSync} from 'react-icons/fa';
+import router from '../scripts/router';
+import mergeSort from '../scripts/mergeSort';
 import '../styles/visualizer.css';
 
 export default class Visualizer extends Component{
@@ -8,14 +11,23 @@ export default class Visualizer extends Component{
         super(props);
         this.state = {
             array: [], // Will be the array used for sorting algorithms
-            arraySize: 100
+            arraySize: 100,
+            radioValue: 1,
+            setRadioValue: 1
         }
         this.generateArray = this.generateArray.bind(this);
+        this.runMergeSort = this.runMergeSort.bind(this);
     }
 
     componentDidMount(){
         // Will generate the initial array once the page loads
         this.generateArray();
+    }
+
+    runMergeSort(){
+        var arr = this.state.array;
+        arr = mergeSort(arr);
+        this.setState({arr});
     }
 
     generateArray(){
@@ -26,11 +38,12 @@ export default class Visualizer extends Component{
             // https://stackoverflow.com/a/7228322
             array.push(Math.floor(Math.random() * (1000 - 3 + 1) + 3));
         }
-        this.setState({array});
+        this.setState({array}); 
     }
 
     render(){
         const mappingArray = this.state.array;
+
         return(
             <div>
             {/* Render array values to divs */}
@@ -43,18 +56,28 @@ export default class Visualizer extends Component{
                 </div>
                 )
             )}
-            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            {/* Controls for sorting and array refresh */}
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" fixed="bottom">
                 <Container>
-                    <Button variant="info" onClick={() => {this.generateArray()}}>Refresh Array</Button>
-                    <Button variant="success">Sort</Button>
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                    <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className="me-auto">
-                            <Button variant="info">test</Button>
-                            <Nav.Link href="#pricing">test</Nav.Link>
-                        </Nav>
-                        
-                    </Navbar.Collapse>
+                    <Button variant="info" onClick={() => {this.generateArray()}}><FaSync /></Button>
+                    <Button variant="success" onClick={() => {this.runMergeSort()}}><FaPlayCircle /></Button>
+                    <select class="algMenu" id ="algMenu">
+                        {/* <option
+                            value='0'
+                        >Select Algorithm</option> */}
+                        <option
+                            value='1'
+                        >Merge sort</option>
+                        <option 
+                            value='2'
+                        >Quick sort</option>
+                        <option 
+                            value='3'
+                        >Heap sort</option>
+                        <option 
+                            value='4'
+                        >Bubble sort</option>
+                    </select>
                 </Container>
             </Navbar>
             
