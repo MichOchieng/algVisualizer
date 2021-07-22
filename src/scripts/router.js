0566eafeda74912ecef23fa974166b25be90d14e
MichOchieng <MichOchieng@gmail.com>
2021-07-21 22:07:19 -0700

    First commit

---
+++ src/scripts/router.js
@@ -0,0 +1,21 @@
+export default function router(){
+   var alg = document.getElementById('algMenu').value;
+   
+   switch (alg) {
+       case '1':
+           console.log("Merge sort selected");
+           break;
+       case '2':
+           console.log("Quick sort selected");
+           break;
+       case '3':
+           console.log("Heap sort selected");
+           break;
+       case '4':
+           console.log("Bubble sort selected");  
+           break;           
+       default:
+           console.log("No algorithm selected");
+           break;
+   }
+}
