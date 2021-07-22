6495ccdce00b13d84f80bcf933cbea5918bbe232
MichOchieng <MichOchieng@gmail.com>
2021-07-21 23:32:53 -0700

    formatting

--- src/scripts/mergeSort.js
+++ src/scripts/mergeSort.js
@@ -17,9 +17,11 @@ function merge(left,right){
     while ((left.length != 0) && (right.length != 0)){
         if (left[0] < right[0]) {
             result.push(left.shift());
+            
         } else {
             result.push(right.shift());
         }
     }
     return [...result,...left,...right];
 }
+
