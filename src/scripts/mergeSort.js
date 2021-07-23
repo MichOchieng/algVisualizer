1a66c28175ddeec82c426f0693835fe50977a155
MichOchieng <MichOchieng@gmail.com>
2021-07-22 18:34:32 -0700

    Completed revised merge sort

--- src/scripts/mergeSort.js
+++ src/scripts/mergeSort.js
@@ -44,30 +44,79 @@
 //  Should be easier to visualize sorting process 
 
 export default function mergeSort(arr){
-    const events = []; // Used for visual events during the sorting process
+    const eventStack = []; // Used for visual events during the sorting process
     if (arr.length <= 1) {
         return arr;
     }
     const tempArr = arr.slice();  // Clone used for overwritting values freely
-    preMerge(arr,tempArr,0,arr.length,events)
-    return events;
+    const n = arr.length - 1;
+    preMerge(arr,tempArr,0,n,eventStack)
+    return eventStack;
 }
 
-function preMerge(arr,tempArr,currentPos,n,events) {
-    if (currentPos == n) {
+function preMerge(arr,tempArr,currentPos,n,eventStack) {
+    if (currentPos === n) {
         return; // Stop once at the end of the array
     }
-    const midpoint = Math.floor(arr.length/2);
-    preMerge(arr,tempArr,currentPos,midpoint,events);
-    preMerge(arr,tempArr,(midpoint + 1),(n - 1),events);
-    merge();
+    const midpoint = Math.floor((currentPos + n)/2);
+    preMerge(arr,tempArr,currentPos,midpoint,eventStack);
+    preMerge(arr,tempArr,(midpoint + 1),n,eventStack);
+    merge(arr,tempArr,eventStack,currentPos,midpoint,n);
 }
 
-function merge(arr,tempArr,events,currentPos,midpoint,n){
+function merge(arr,tempArr,eventStack,currentPos,midpoint,n){
     // Push values to the event array when:
         // Comparing values
         // Swapping values
 
-    
+    var x,y = currentPos;
+    var z   = (midpoint + 1);
+
+    // Left and Right
+    while (x <= midpoint && z <= n ) {
+        // Comparing values - Highlight
+        eventStack.push([x,z]);
+        // Comparing values - Revert Colour
+        eventStack.push([x,z]);
+        if(tempArr[x] <= tempArr[z]){
+            // Overwrite value with value at tempArr[x]
+            eventStack.push([y,tempArr[x]]);
+            arr[y++] = tempArr[x++];
+        }else{
+            // Overwrite value with value at tempArr[z]
+            eventStack.push([y,tempArr[z]]);
+            arr[y++] = tempArr[z++];
+        }
+    }
+    // Left Side
+    while (x <= midpoint ) {
+        // Comparing values - Highlight
+        eventStack.push([x,x]);
+        // Comparing values - Revert Colour
+        eventStack.push([x,x]);
+        // Overwrite value
+        eventStack.push([y,tempArr[x]]);
+        arr[y++] = tempArr[x++];
+    }
+    // Right Side
+    while (z <= n) {
+        // Comparing values - Highlight
+        eventStack.push([z,z]);
+        // Comparing values - Revert Colour
+        eventStack.push([z,z]);
+        // Overwrite value
+        eventStack.push([y,tempArr[z]]);
+        arr[y++] = tempArr[z++];
+    }
 }
 
+// Causes infinite loop
+function partitionPush(x,y,arr,tempArr,eventStack){
+    // Comparing values - Highlight
+    eventStack.push([x,x]);
+    // Comparing values - Revert Colour
+    eventStack.push([x,x]);
+    // Overwrite value
+    eventStack.push([y,tempArr[x]]);
+    arr[y++] = tempArr[x++];
+}   
